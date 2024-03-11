d06a5e565e377eb8ec0f8bfbf2f5fbfa4851e437
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-03-11 17:46:32 +0000

    Have finite angle

--- test/pyflubl/test_T002_RBend.py
+++ test/pyflubl/test_T002_RBend.py
@@ -1,8 +1,11 @@
 import pyflubl as _pfbl
+import numpy as _np
 
 def test_T002_rbend() :
     m = _pfbl.Builder.Machine()
     m.AddDrift(name="d1", length=1)
-    m.AddRBend(name="rb1", length=1, angle=0.0, bendxsize=0.75, bendysize=0.75)
+    m.AddRBend(name="rb1", length=1, angle=_np.pi/4, bendxsize=0.75, bendysize=0.75)
     m.AddSamplerPlane(name="s1", length=1e-6, samplersize=1)
     m.Write("T002_rbend")
+
+    return m
