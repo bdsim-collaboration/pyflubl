5c5b02f69ecd7383f7beb4859751e461cead264a
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-03-31 20:58:18 +0100

    r and sbend ring test

---
+++ test/pyflubl/test_T110_Ring_RBend.py
@@ -0,0 +1,19 @@
+import pyflubl as _pfbl
+import numpy as _np
+
+def test_T110_Ring_RBend() :
+    m = _pfbl.Builder.Machine()
+
+    n = 20
+    bendangle = 2.*_np.pi/n
+    print(bendangle)
+    for i in range(0,n,1):
+        m.AddDrift(name="d1-"+str(i), length=0.5,
+                   beampipeMaterial="G4_STAINLESS-STEEL",
+                   beampipeRadius=30, beampipeThickness=5)
+        m.AddRBend(name="rb_"+str(i), length=0.5, angle=bendangle)
+        m.AddSamplerPlane(name="s1_"+str(i), length=1e-6)
+
+    m.Write("T110_Ring_RBend")
+
+    return m
--- test/pyflubl/test_T111_Ring_SBend.py
+++ test/pyflubl/test_T111_Ring_SBend.py
@@ -1,7 +1,7 @@
 import pyflubl as _pfbl
 import numpy as _np
 
-def test_T110_Ring() :
+def test_T111_Ring_SBend() :
     m = _pfbl.Builder.Machine()
 
     n = 20
@@ -12,8 +12,8 @@ def test_T110_Ring() :
                    beampipeMaterial="G4_STAINLESS-STEEL",
                    beampipeRadius=30, beampipeThickness=5)
         m.AddSBend(name="rb_"+str(i), length=0.5, angle=bendangle)
-        m.AddSamplerPlane(name="s1_"+str(i), length=1e-6, samplersize=1)
+        m.AddSamplerPlane(name="s1_"+str(i), length=1e-6)
 
-    m.Write("T110_Ring")
+    m.Write("T111_Ring_SBend")
 
     return m
