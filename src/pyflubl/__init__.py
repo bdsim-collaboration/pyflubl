5f4830493ed7dd5e6cc2e3fe48ec5d254efab6d2
Laurie Nevay <laurie.nevay@cern.ch>
2024-05-17 00:49:46 +0200

    Tidy imports.

--- src/pyflubl/__init__.py
+++ src/pyflubl/__init__.py
@@ -2,8 +2,8 @@ from . import Builder
 from . import Convert
 from .Beam import *
 from .Defaults import *
+#from .FlukaMachine import FlukaMachine
 from .Run import *
 from .Usr import *
 
-# from .FlukaMachine import FlukaMachine
-
+__all__ = ["Beam", "Builder", "Convert", "Defaults", "Run", "Usr"]
