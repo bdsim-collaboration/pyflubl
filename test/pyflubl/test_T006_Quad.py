d182b81b46a797174abdeff006d76ffd50be27f9
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-04-18 21:33:58 +0100

    remove offset and tilt of quad

--- test/pyflubl/test_T006_Quad.py
+++ test/pyflubl/test_T006_Quad.py
@@ -21,7 +21,7 @@ def test_T006_quad() :
     m.AddDrift(name="d1", length=1, beampipeMaterial = "G4_STAINLESS-STEEL",
                beampipeRadius=30, beampipeThickness=5)
     m.AddSamplerPlane(name="s1", length=1e-6, samplersize=1)
-    m.AddQuadrupole(name="q1", length=0.5, k1=0.5, tilt=_np.pi/4, offsetX=500, offsetY=500)
+    m.AddQuadrupole(name="q1", length=0.5, k1=0.5, tilt=0.0, offsetX=0.0, offsetY=0.0)
     m.AddSamplerPlane(name="s2", length=1e-6, samplersize=1)
     m.AddDrift(name="d2", length=1, beampipeMaterial = "G4_STAINLESS-STEEL",
                beampipeRadius=30, beampipeThickness=5)
