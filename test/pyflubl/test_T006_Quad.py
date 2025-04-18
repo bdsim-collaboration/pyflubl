66d22b419991d87eb6ced71059602567893d18dc
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-04-18 22:19:27 +0100

    specific tests for tilt, offset(X/Y)

--- test/pyflubl/test_T006_Quad.py
+++ test/pyflubl/test_T006_Quad.py
@@ -1,9 +1,8 @@
 import pyflubl as _pfbl
 import numpy as _np
 
-def test_T006_quad() :
+def quad_basic(tilt = 0, offsetX = 0, offsetY = 0, fileName = "T006_Quad"):
     m = _pfbl.Builder.Machine(bakeTransforms=True)
-
     d = _pfbl.Defaults('EM-CASCA')
     m.AddDefaults(d)
 
@@ -21,10 +20,22 @@ def test_T006_quad() :
     m.AddDrift(name="d1", length=1, beampipeMaterial = "G4_STAINLESS-STEEL",
                beampipeRadius=30, beampipeThickness=5)
     m.AddSamplerPlane(name="s1", length=1e-6, samplersize=1)
-    m.AddQuadrupole(name="q1", length=0.5, k1=0.5, tilt=0.0, offsetX=0.0, offsetY=0.0)
+    m.AddQuadrupole(name="q1", length=0.5, k1=0.5, tilt=tilt, offsetX=offsetX, offsetY=offsetY)
     m.AddSamplerPlane(name="s2", length=1e-6, samplersize=1)
     m.AddDrift(name="d2", length=1, beampipeMaterial = "G4_STAINLESS-STEEL",
                beampipeRadius=30, beampipeThickness=5)
-    m.Write("T006_Quad")
+    m.Write(fileName)
 
     return m
+
+def test_T006_quad() :
+    quad_basic()
+
+def test_T006_quad_tilt() :
+    quad_basic(tilt=_np.pi/4, fileName="T006_Quad_tilt")
+
+def test_T006_quad_offsetX() :
+    quad_basic(offsetX=50, fileName="T006_Quad_offsetX")
+
+def test_T006_quad_offsetY() :
+    quad_basic(offsetY=50, fileName="T006_Quad_offsetY")
