3caf9f7c4611b2326b31da68475a481a6e8fe0e6
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-04-25 17:39:18 +0100

    analysis : add rectangles for elements placed in the beamline

--- src/pyflubl/Analysis/_plot.py
+++ src/pyflubl/Analysis/_plot.py
@@ -2,7 +2,10 @@ from ._usrbin import *
 from ._usrdump import *
 import numpy as _np
 
+from pyg4ometry.transformation import matrix2tbxyz as _matrix2tbxyz
+
 from matplotlib import pyplot as _plt
+from matplotlib import patches as _patches
 
 def plot(data):
     if type(data) == Usrbin :
@@ -33,31 +36,55 @@ def plot_usrbin(ub, detector_idx = 0, projection = 0):
 def plot_machine(machine) :
     m = machine
 
-    for ekey in m.elements :
-        e  = m.elements[ekey]
+    for eidx in range(0,len(m.sequence),1) :
+        ename  = m.sequence[eidx]
+        e = m.elements[ename]
 
-        if e.name in m.elementBookkeeping :
-            einfo = m.elementBookkeeping[e.name]
-        else :
-            print("element has no bookkeeping info", e.name)
-            continue
+        x, y, z = [1000*v for v in m.midint[eidx]]
+        xr, yr, zr  = _matrix2tbxyz(_np.array(m.midrotationint[eidx]))
 
-        _plt.subplot(3,1,1)
-        _plt.plot(einfo['translation'][2], einfo['translation'][0],"+")
+        print(z,y,z,xr,yr,zr)
+
+        length  = e.length
+        width = 250
+
+        ###########################################
+        ax = _plt.subplot(3,1,1)
+        _plt.plot(z,x,"+")
+        bounding_box = _makeBoundingRect([z,x], [length*1000, width], yr)
+        ax.add_patch(bounding_box)
+
         _plt.xlabel("z/mm")
         _plt.ylabel("x/mm")
 
-        _plt.subplot(3,1,2)
-        _plt.plot(einfo['translation'][2], einfo['translation'][1],"+")
+        ###########################################
+        ax = _plt.subplot(3,1,2)
+        _plt.plot(z, y,"+")
+        bounding_box = _makeBoundingRect([z,y], [length*1000, width], xr)
+        ax.add_patch(bounding_box)
+
         _plt.xlabel("z/mm")
         _plt.ylabel("y/mm")
 
-        _plt.subplot(3,1,3)
-        _plt.plot(einfo['translation'][0], einfo['translation'][1],"+")
+        ###########################################
+        ax = _plt.subplot(3,1,3)
+        _plt.plot(x, y,"+")
+        bounding_box = _makeBoundingRect([x,y], [width, width], zr)
+        ax.add_patch(bounding_box)
+
         _plt.xlabel("x/mm")
         _plt.ylabel("y/mm")
 
+        ###########################################
         _plt.tight_layout()
 
+def _makeBoundingRect(centre, size, angle) :
+    cen = _np.array(centre)
+    size  = _np.array(size)
+    rr = _np.array([[_np.cos(angle), -_np.sin(angle)],
+                    [_np.sin(angle),  _np.cos(angle)]])
+    ll = cen - rr @ size/2
 
+    return  _patches.Rectangle(ll, size[0], size[1],
+                               angle=angle / _np.pi * 180, fill=False)
 
