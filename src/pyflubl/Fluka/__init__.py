ac4251fce6c5ab21b97780bdbeecf84cc66da759
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-05-04 17:59:27 +0100

    Fluka : Add rotprbin and usrbin

--- src/pyflubl/Fluka/__init__.py
+++ src/pyflubl/Fluka/__init__.py
@@ -3,10 +3,12 @@ from ._defaults import *
 from ._global import *
 from ._mgn import *
 from ._randomiz import *
+from ._rotprbin import *
 from ._start import *
 from ._title import *
 from ._usrtrack import *
 from ._usrbdx import *
+from ._usrbin import *
 from ._userdump import *
 
 from .Beam import *
