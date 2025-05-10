f889845099b9af7be257ef14686ecccb25a92ae9
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-05-10 23:04:15 +0100

    FlukaC++ : Start of sampler data

---
+++ src/fluka/4-4.0/include/SamplerData.h
@@ -0,0 +1,17 @@
+
+class SamplerData {
+    public :
+        SamplerData() {
+            nparticle = 0;
+        };
+        ~SamplerData() {};
+
+        int nparticle;
+        double x;
+        double y;
+        double z;
+
+        double xp;
+        double yp;
+        double zp;
+};
