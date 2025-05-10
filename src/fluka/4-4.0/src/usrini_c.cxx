374e2806c6d6841d23ccce7dd19c58d6e8a08ed3
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-05-10 23:27:30 +0100

    JSON : refactor

--- src/fluka/4-4.0/src/usrini_c.cxx
+++ src/fluka/4-4.0/src/usrini_c.cxx
@@ -1,11 +1,15 @@
+#include <fstream>
 #include <iostream>
 #include "global_cxx.h"
+#include <nlohmann/json.hpp>
 
 TFile *outputFile = nullptr;
 TTree *eventTree = nullptr;
 SamplerData *samplers = nullptr;
 
 int iEvt = 0;
+
+using json = nlohmann::json;
 
 extern "C" {
     void usrini_c_();  // Fortran 77 adds underscore
@@ -25,6 +29,11 @@ void usrini_c_() {
 
 void loadBookkeeping() {
     std::cout << "loadBookkeeping>" << std::endl;
+    std::ifstream f("/tmp/pyflubl/test/pyflubl/run_T300_Usricall/T300_Usricall.json");
+    if(!f) {
+        std::cerr << "Could not open JSON file." << std::endl;
+    }
+    json data = json::parse(f);
 }
 
 void openRootFile() {
