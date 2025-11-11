1107db50cdf58dd8049ea53be4d0f7a00fda23f3
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-11-11 09:26:25 +0000

    Test : search for input in correct relative directory

--- test/pyflubl/test_T032_Custom.py
+++ test/pyflubl/test_T032_Custom.py
@@ -1,6 +1,7 @@
 import pyflubl as _pfbl
 import pyg4ometry as _pyg4
 import numpy as _np
+import os as _os
 
 def test_T032_custom_G4() :
     m = _pfbl.Builder.Machine(bakeTransforms=True)
@@ -39,6 +40,8 @@ def test_T032_custom_G4() :
     m.Write("T032_Custom_G4")
 
 def test_T032_custom_G4_File() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
     m = _pfbl.Builder.Machine(bakeTransforms=True)
 
     d = _pfbl.Fluka.Defaults('EM-CASCA')
@@ -61,11 +64,13 @@ def test_T032_custom_G4_File() :
     m.AddDrift(name="d1", length=1)
     m.AddSBend(name="b1", length=1, angle=_np.pi/6)
     m.AddDrift(name="d2", length=1)
-    m.AddCustomG4File(name="c1", length=1, geometryFile="./geometryInput/test_T032_Custom_Pyg4.gdml", lvName="bl")
+    m.AddCustomG4File(name="c1", length=1, geometryFile=this_dir+"/geometryInput/test_T032_Custom_Pyg4.gdml", lvName="bl")
     m.AddSamplerPlane(name="s1", length=1e-6)
     m.Write("T032_Custom_G4_File")
 
 def test_T032_custom_Fluka() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
     m = _pfbl.Builder.Machine(bakeTransforms=True)
 
     d = _pfbl.Fluka.Defaults('EM-CASCA')
@@ -86,7 +91,7 @@ def test_T032_custom_Fluka() :
     m.AddStart(s)
 
     # custom fluka geometry
-    reader = _pyg4.fluka.Reader("./geometryInput/test_T032_Custom_Fluka_Gap.inp")
+    reader = _pyg4.fluka.Reader(this_dir+"/geometryInput/test_T032_Custom_Fluka_Gap.inp")
     registry = reader.getRegistry()
 
     outer_bodies = [registry.bodyDict[k] for k in ['outer']]
@@ -106,6 +111,8 @@ def test_T032_custom_Fluka() :
     m.Write("T032_Custom_Fluka")
 
 def test_T032_custom_Fluka_File() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
     m = _pfbl.Builder.Machine(bakeTransforms=True)
 
     d = _pfbl.Fluka.Defaults('EM-CASCA')
@@ -126,7 +133,7 @@ def test_T032_custom_Fluka_File() :
     m.AddStart(s)
 
     # custom fluka geometry
-    reader = _pyg4.fluka.Reader("./geometryInput/test_T032_Custom_Fluka_Gap.inp")
+    reader = _pyg4.fluka.Reader(this_dir+"/geometryInput/test_T032_Custom_Fluka_Gap.inp")
     registry = reader.getRegistry()
 
     outer_bodies = [registry.bodyDict[k] for k in ['outer']]
@@ -135,7 +142,7 @@ def test_T032_custom_Fluka_File() :
     m.AddDrift(name="d1", length=1)
     m.AddSBend(name="b1", length=1, angle=_np.pi/8)
     m.AddDrift(name="d2", length=1)
-    m.AddCustomFlukaFile(name="c1", length=1, geometryFile="./geometryInput/test_T032_Custom_Fluka_Gap.inp",
+    m.AddCustomFlukaFile(name="c1", length=1, geometryFile=this_dir+"/geometryInput/test_T032_Custom_Fluka_Gap.inp",
                          customOuterBodies="outer",
                          customRegions="OUTER SHIELD BEAM TARGET")
     m.AddSamplerPlane(name="s1", length=1e-6)
