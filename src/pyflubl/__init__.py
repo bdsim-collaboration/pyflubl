fe6eb3a2253de77092ef726617ecf76f2ddbcd41
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-11-23 16:52:32 +0000

    docs : version number

--- src/pyflubl/__init__.py
+++ src/pyflubl/__init__.py
@@ -1,3 +1,10 @@
+try:
+    from ._version import __version__
+    from ._version import __version_tuple__
+except ImportError:
+    __version__ = "unknown version"
+    __version_tuple__ = (0, 0, "unknown version")
+
 from .Options import Options
 from . import Builder
 from . import Convert
