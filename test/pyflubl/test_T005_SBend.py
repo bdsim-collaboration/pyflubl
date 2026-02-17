706639699a3c0a34486c4dfc007981ad8388d410
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-02-17 15:21:24 +0000

    test : simple 90 degree example

--- test/pyflubl/test_T005_SBend.py
+++ test/pyflubl/test_T005_SBend.py
@@ -55,7 +55,7 @@ def make_T005_sbend() :
                       length=1e-6)
     m.AddSBend(name="sb2",
                length=1,
-               angle=-_np.pi /18)
+               angle=-_np.pi/18)
     m.AddSamplerPlane(name="s4",
                       length=1e-6)
     m.AddDrift(name="d3",
@@ -215,7 +215,7 @@ def make_T005_sbend_split_tilt() :
     d = _pfbl.Fluka.Defaults('EM-CASCA')
     m.AddDefaults(d)
 
-    b = _pfbl.Fluka.Beam1(momentumOrKe=1, energySpread=0.01, sdum="ELECTRON")
+    b = _pfbl.Fluka.Beam1(momentumOrKe=1, energySpread=0.00, sdum="ELECTRON")
     bp = _pfbl.Fluka.Beampos(xCentre=0, yCentre=0, zCentre=0, xCosine=0, yCosine=0)
     ba = _pfbl.Fluka.BeamAxes(xxCosine=1, xyCosine=0, xzCosine=0,
                               zxCosine=0, zyCosine=0, zzCosine=1)
@@ -250,7 +250,8 @@ def make_T005_sbend_split_tilt() :
     m.AddSBendSplit(name="sb1",
                     length=2,
                     angle=_np.pi/4,
-                    tilt=_np.pi/2, nsplit=10)
+                    tilt=_np.pi/2,
+                    nsplit=10)
     m.AddSamplerPlane(name="s2",
                       length=1e-6)
     m.AddDrift(name="d2",
@@ -264,6 +265,56 @@ def make_T005_sbend_split_tilt() :
 
 def test_T005_sbend_split_tilt() :
     make_T005_sbend_split_tilt()
+
+def make_T005_sbend_90deg() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
+    m = _pfbl.Builder.Machine(bakeTransforms=True)
+
+    d = _pfbl.Fluka.Defaults('EM-CASCA')
+    m.AddDefaults(d)
+
+    b = _pfbl.Fluka.Beam1(momentumOrKe=1, energySpread=0, sdum="ELECTRON")
+    bp = _pfbl.Fluka.Beampos(xCentre=0, yCentre=0, zCentre=0, xCosine=0, yCosine=0)
+    ba = _pfbl.Fluka.BeamAxes(xxCosine=1, xyCosine=0, xzCosine=0,
+                              zxCosine=0, zyCosine=0, zzCosine=1)
+    m.AddBeam1(b)
+    m.AddBeampos(bp)
+    m.AddBeamaxes(ba)
+
+    r = _pfbl.Fluka.Randomiz()
+    m.AddRandomiz(r)
+
+    s = _pfbl.Fluka.Start(500)
+    m.AddStart(s)
+
+    uic = _pfbl.Fluka.Usricall()
+    m.AddUsricall(uic)
+
+    uoc = _pfbl.Fluka.Usrocall()
+    m.AddUsrocall(uoc)
+
+    ud = _pfbl.Fluka.Userdump(mgdraw=100,lun=23,mgdrawOption=-1,userDump=None, outputFile="dump")
+    m.AddUserdump(ud)
+
+    # set world material
+    m.world_material = "VACUUM"
+
+    m.AddSamplerPlane(name="s1",
+                      length=1e-6)
+    m.AddSBendSplit(name="sb1",
+                    length=7.853981633974483,
+                    angle=_np.pi/2,
+                    nsplit=10)
+    m.AddSamplerPlane(name="s2",
+                      length=1e-6)
+
+    m.Write(this_dir+"/T005_SBend_90deg")
+
+    return m
+
+def test_T005_sbend_90deg() :
+    make_T005_sbend_90deg()
 
 if __name__ == "__main__":
     test_T005_sbend()
