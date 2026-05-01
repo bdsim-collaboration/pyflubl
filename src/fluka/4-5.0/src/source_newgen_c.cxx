dbc87da3dbb88bde014e08787e12cd0656abe840
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-05-01 21:58:31 +0100

    Forgot C++ files

---
+++ src/fluka/4-5.0/src/source_newgen_c.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+
+extern "C" {
+    void source_newgen_init_c_();
+    void source_newgen_c_();
+}
+
+void source_newgen_init_c_() {
+    std::cout << "source_newgen_init_c> " << std::endl;
+}
+
+void source_newgen_c_() {
+    std::cout << "source_newgen_c> " << std::endl;
+
+}
