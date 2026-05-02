f77267389cfe123f746d72825c916d9307747501
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-05-02 10:29:11 +0100

    syntax error

--- src/fluka/4-5.0/src/source_newgen_c.cxx
+++ src/fluka/4-5.0/src/source_newgen_c.cxx
@@ -15,11 +15,11 @@ void source_newgen_c_(double *whasou, int *particle_code) {
     std::cout << "source_newgen_c> " << whasou[0] << std::endl;
     if (whasou[0] == 1) {
       source_newgen_twiss_c_(whasou[1], whasou[2], whasou[3], whasou[4], whasou[5],)
-                              whasou[6], whasou[7], whasou[8], whasou[9], whasou[10]);
+                             whasou[6], whasou[7], whasou[8], whasou[9], whasou[10]);
     }
 }
 
-void source_newgen_twiss_c_(double emitx, double alpx, double betx, double etax, double etaxp
+void source_newgen_twiss_c_(double emitx, double alpx, double betx, double etax, double etaxp,
                             double emity, double alpy, double bety, double etay, double etayp) {
     std::cout << "source_newgen_twiss_c> " << emitx << " " << alpx << std::endl;
 }
