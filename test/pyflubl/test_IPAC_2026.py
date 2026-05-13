ade78f12a8692c7702bf2ea00633a653090700c3
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-05-13 14:09:55 +0100

    Simple magnetic tracking example

--- test/pyflubl/test_IPAC_2026.py
+++ test/pyflubl/test_IPAC_2026.py
@@ -44,6 +44,80 @@ def IPAC_2026_Lattice() :
 
     m.Write(this_dir + "/IPAC_2026_Lattice")
 
+def IPAC_2026_magnetic() :
+
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
+    m = _pfbl.BuilderNew.Machine(bakeTransforms=True)
+
+    d = _pfbl.Fluka.Defaults('EM-CASCA')
+    m.AddDefaults(d)
+
+    b = _pfbl.Fluka.Beam(momentumOrKe=1, energySpread=0, sdum="ELECTRON")
+    bp = _pfbl.Fluka.Beampos(xCentre=0.5, yCentre=0, zCentre=0, xCosine=0, yCosine=0)
+    ba = _pfbl.Fluka.BeamAxes(xxCosine=1, xyCosine=0, xzCosine=0,
+                              zxCosine=0, zyCosine=0, zzCosine=1)
+
+    m.AddBeam(b)
+    m.AddBeampos(bp)
+    m.AddBeamaxes(ba)
+
+    r = _pfbl.Fluka.Randomiz()
+    m.AddRandomiz(r)
+
+    s = _pfbl.Fluka.Start(1)
+    m.AddStart(s)
+
+    uic = _pfbl.Fluka.Usricall()
+    m.AddUsricall(uic)
+
+    uoc = _pfbl.Fluka.Usrocall()
+    m.AddUsrocall(uoc)
+
+    ud = _pfbl.Fluka.Userdump(mgdraw=100, lun=23, mgdrawOption=-1, userDump=None, outputFile="dump")
+    m.AddUserdump(ud)
+
+    # Make materials (SS 316 )
+    fr = m._GetFlukaRegistry(True)
+    fm_c = _pyg4.fluka.Material("C", 6, 2.266, 12, flukaregistry=fr)  # graphite density
+    fm_mn = _pyg4.fluka.Material("MN", 25, 7.21, 55, flukaregistry=fr)  # density range 7.21 - 7.47 g/cm^3
+    fm_p = _pyg4.fluka.Material("P", 15, 1.82, 31, flukaregistry=fr)  # white 1.82 g/cm^3
+    fm_s = _pyg4.fluka.Material("S", 16, 2.07, 32, flukaregistry=fr)
+    fm_si = _pyg4.fluka.Material("SI", 14, 2.33, 28, flukaregistry=fr)
+    fm_cr = _pyg4.fluka.Material("CR", 24, 7.19, 52, flukaregistry=fr)
+    fm_ni = _pyg4.fluka.Material("NI", 28, 8.90, 58, flukaregistry=fr)
+    fm_mo = _pyg4.fluka.Material("MO", 42, 10.22, 96, flukaregistry=fr)
+    fm_fe = _pyg4.fluka.Material("FE", 26, 7.874, 56, flukaregistry=fr)
+    fm_ss = _pyg4.fluka.Compound("SS", 8.0, [(fm_c, 0.08 / 100),
+                                             (fm_mn, 2 / 100),
+                                             (fm_p, 0.045 / 100),
+                                             (fm_s, 0.03 / 100),
+                                             (fm_si, 1 / 100),
+                                             (fm_cr, 16 / 100),
+                                             (fm_ni, 10 / 100),
+                                             (fm_mo, 2 / 100),
+                                             (fm_fe, 68.845 / 100)],
+                                 "mass",
+                                 flukaregistry=fr)
+
+    m.AddDrift(name="d1", length=1, beampipeMaterial="SS")
+    m.AddSamplerPlane(name="s1", length=1e-6)
+    m.AddQuadrupole(name="q1", length=0.25, k1=-0.2, beampipeMaterial="SS")
+    m.AddDrift(name="d2", length=1.0, beampipeMaterial="SS")
+    m.AddSamplerPlane(name="s2", length=1e-6)
+    #m.AddTarget(name="t1", length=0.01, material="IRON")
+    m.AddDrift(name="t1", length=0.01, beampipeMaterial="SS")
+    m.AddSamplerPlane(name="s3", length=1e-6)
+    m.AddDrift(name="d3", length=1.0, beampipeMaterial="SS")
+    m.AddSamplerPlane(name="s4", length=1e-6)
+    m.AddSBendSplit(name="sb1", length=2, angle=0.2, nsplit=10, beampipeMaterial="SS")
+    m.AddSamplerPlane(name="s5", length=1e-6)
+    m.AddDrift(name="d4", length=1.0, beampipeMaterial="SS")
+    m.AddSamplerPlane(name="s6", length=1e-6)
+
+    m.SaveJSON(this_dir + "/IPAC_2026_magnetic_coordinate.json")
+    m.Write(this_dir + "/IPAC_2026_magnetic")
+
 def IPAC_2026() :
 
     this_dir = _os.path.dirname(_os.path.abspath(__file__))
